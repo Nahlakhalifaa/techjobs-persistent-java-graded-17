d655c6f2863a40752bf3199104f12c3761567e4d
Nahlakhalifaa <nahla.kh0025@gmail.com>
2023-11-29 04:46:25 -0600

    Connected with the database.

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -8,11 +8,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
-
+@MappedSuperclass
 public abstract class AbstractEntity {
-
+@Id @GeneratedValue
     private int id;
-
+    @NotNull(message = "Field is required")
+    @Size(min = 3, max = 100, message = "Name must be between 4 and 100 characters")
     private String name;
 
     public int getId() {
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    @Size(min = 4, max = 100, message = "Location must be between 3 and 100 characters")
+    private String location;
+
+    public Employer () {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 }
